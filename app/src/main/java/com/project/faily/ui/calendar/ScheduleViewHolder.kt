00e95c0bf8215b545c4172fe59c26f8fc0c3064d
wsb7788 <wsb0514@naver.com>
2021-10-30 07:49:15 +0900

    calendar recycler backgroundtint color grammatically set

--- app/src/main/java/com/project/faily/ui/calendar/ScheduleViewHolder.kt
+++ app/src/main/java/com/project/faily/ui/calendar/ScheduleViewHolder.kt
@@ -10,6 +10,7 @@ import com.project.faily.databinding.LayoutRecyclerHomePresentBinding
 import com.project.faily.databinding.LayoutRecyclerScheduleBinding
 import com.project.faily.util.SharedPreferencesManager
 import com.project.faily.util.getWeekDay
+import kotlin.coroutines.coroutineContext
 
 class ScheduleViewHolder(val binding: LayoutRecyclerScheduleBinding): RecyclerView.ViewHolder(binding.root) {
 
@@ -17,10 +18,10 @@ class ScheduleViewHolder(val binding: LayoutRecyclerScheduleBinding): RecyclerVi
     fun bind(scheduleModel: ScheduleModel){
 
         when(scheduleModel.category){
-            "기념일" -> binding.clOut.setBackgroundResource(R.drawable.box_schedule_anniversary)
-            "가족" -> binding.clOut.setBackgroundColor(R.color.primary_pink)
-            "개인" -> binding.clOut.setBackgroundColor(R.color.light_grey_green)
-            "일반" -> binding.clOut.setBackgroundColor(R.color.secondary_pink)
+            "기념일" ->  binding.clOut.backgroundTintList = null
+            "가족" -> binding.clOut.backgroundTintList = ApplicationClass.instance.getColorStateList(R.color.primary_pink)
+            "개인" -> binding.clOut.backgroundTintList = ApplicationClass.instance.getColorStateList(R.color.light_grey_green)
+            "일반" -> binding.clOut.backgroundTintList = ApplicationClass.instance.getColorStateList(R.color.secondary_pink)
         }
 
         binding.tvScheduleDay.text = scheduleModel.date.substring(8,10)
