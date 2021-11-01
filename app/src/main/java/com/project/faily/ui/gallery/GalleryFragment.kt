ac3cfc47916c68f2fe4f0c6e858f4a9980068508
wsb7788 <wsb0514@naver.com>
2021-11-02 06:10:08 +0900

    갤러리 야매 구현

--- app/src/main/java/com/project/faily/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/project/faily/ui/gallery/GalleryFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.project.faily.ApplicationClass
 import com.project.faily.R
 import com.project.faily.data.remote.gallery.GalleryListener
 import com.project.faily.data.remote.home.HomeListener
@@ -28,6 +30,18 @@ class GalleryFragment : Fragment(), GalleryListener {
         binding.viewModel = viewModel
         viewModel.galleryListener = this
 
+        Glide
+            .with(ApplicationClass.instance)
+            .load(R.drawable.profile_sb)
+            .circleCrop()
+            .placeholder(R.drawable.ic_profile_basic)
+            .into(binding.ivProfile)
+
+
+        binding.iv1.setImageResource(R.drawable.asdfasdf1)
+        binding.iv2.setImageResource(R.drawable.asdfasdf2)
+        binding.iv3.setImageResource(R.drawable.asdfasdf3)
+        binding.iv4.setImageResource(R.drawable.asdfasdf4)
         return binding.root
     }
 
